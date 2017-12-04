959a9cf5cec5416256703ac9e51eee71db5e0fa1
Ellen Dai <ellen.dai@wustl.edu>
2017-12-04 09:37:23 -0600

    Images display, always the correct number of times, and are always there after routing.  Life is getting better.  Now have to remove image from galleries upon deleting.

--- imports/ui/components/uploads-grid.js
+++ imports/ui/components/uploads-grid.js
@@ -2,8 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import {ReactiveDict} from 'meteor/reactive-dict';
 import { Uploads } from '../../api/uploads.js';
+import { Router } from 'meteor/iron:router';
 
-//import '../components/upload-box.js';
 import './uploads-grid.html';
 
 Template.uploads_grid.onCreated(function() {
@@ -17,61 +17,6 @@ Template.uploads_grid.onRendered(function() {
 Template.uploads_grid.helpers({
   uploads: function() {
     return Uploads.find({owner: Meteor.userId()});
-  },
-  loadUploads() {
-    //if (TempTemplate.instance().find('#ug_body_grid')) {
-    //  //NOT WORKING WHYYYYY
-    //  Template.instance().find('#ug_grid_body').innerHTML = ''; // clear table
-    //}
-    if (! Meteor.userId()) {
-      alert("you are not logged in.");
-      throw new Meteor.Error('not-authorized');
-    }
-    var foundUploads = Uploads.find({owner: Meteor.userId()}); // cursor object
-    var i = 0;
-    console.log(foundUploads.fetch());
-    //alert("foundUploads length: " + foundUploads.count());
-    var newRow;
-    var newEntry;
-    var newEntryHyperlink;
-    var newEntryImage;
-    var newEntryEdit;
-    var newEntryDelete;
-    foundUploads.forEach(function(theUpload) {
-      if (i % 4 === 0) {
-        if (newRow !== undefined) {
-          Template.instance().find('#ug_grid_body').appendChild(newRow);
-        }
-        newRow = document.createElement('tr');
-      }
-      //console.log(theUpload.url());
-      newEntry = document.createElement('td');
-      newEntry.setAttribute('class', 'upload_cell');
-      newEntryHyperlink = document.createElement('a');
-      newEntryHyperlink.setAttribute('href', theUpload.url('imageStore'));
-      newEntryHyperlink.setAttribute('target', '_blank');
-      newEntryImage = document.createElement('img');
-      newEntryImage.setAttribute('src', theUpload.url());
-      newEntryImage.setAttribute('alt', "cannot display image");
-      newEntryImage.setAttribute('class', "imageInUploadsGrid");
-      newEntryHyperlink.appendChild(newEntryImage);
-      newEntry.appendChild(newEntryHyperlink);
-      newEntry.innerHTML += "<br>Caption: " + theUpload.caption + "<br>Owner: " + theUpload.owner + "<br>";
-      //TODO: make edit button work
-      newEntryEdit = document.createElement('button');
-      newEntryEdit.innerHTML = "Edit";
-      newEntryEdit.setAttribute('value', theUpload._id);
-      newEntryEdit.setAttribute('class', "edit_button");
-      newEntry.appendChild(newEntryEdit);
-      newEntryDelete = document.createElement('button');
-      newEntryDelete.innerHTML = "Delete";
-      newEntryDelete.setAttribute('value', theUpload._id);
-      newEntryDelete.setAttribute('class', "delete_button");
-      newEntry.appendChild(newEntryDelete);
-      newRow.appendChild(newEntry);
-      i++;
-    });
-    Template.instance().find('#ug_grid_body').appendChild(newRow);
   }
 });
 
@@ -82,9 +27,14 @@ Template.uploads_grid.events({
   //  alert("upload button clicked.  Will add modal later.");
   //  //$('#upload-box').dialog('show');
   //}
+  'click #goto-upload'(event) {
+    event.preventDefault();
+    console.log(event);
+    Router.go('/uploadForm');
+  }
 });
 
-Template.imgBox.events({
+Template.imgBoxU.events({
   'click .delete_button'(event) {
     event.preventDefault();
     var uploadId = event.target.value;
