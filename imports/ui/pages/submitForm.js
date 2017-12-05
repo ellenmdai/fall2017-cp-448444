26a9200a3bbc4bf087a96538546512a6003eae78
Ellen Dai <ellen.dai@wustl.edu>
2017-12-04 20:25:21 -0600

    New image ui in submit form.  All functionality is grrreat.  TODO: clean code, security, css

--- imports/ui/pages/submitForm.js
+++ imports/ui/pages/submitForm.js
@@ -3,12 +3,10 @@ import { Template } from 'meteor/templating';
 import {ReactiveDict} from 'meteor/reactive-dict';
 import {Router} from 'meteor/iron:router';
 
-import { Galleries } from '../../api/galleries.js';
 import { Uploads } from '../../api/uploads.js';
 import { SubmitRequests } from '../../api/submitrequests.js';
 
 import './submitForm.html';
-//import '../components/uploads-grid.js';
 import '../components/header.js';
 
 Template.submitForm.onCreated(function bodyOnCreated() {
@@ -16,10 +14,6 @@ Template.submitForm.onCreated(function bodyOnCreated() {
   Meteor.subscribe('galleries');
   Meteor.subscribe('uploads');
   Meteor.subscribe('submitrequests');
-});
-
-Template.submitForm.onRendered(function() {
-  
 });
 
 Template.submitForm.helpers({
@@ -30,28 +24,32 @@ Template.submitForm.helpers({
 
 Template.submitForm.events({
   'submit #new_submit_form'(event) {
-	event.preventDefault();
-	console.log(event);
-	var galleryId = Template.parentData(0)._id;
-	console.log(galleryId);
-	var selector = Template.instance().find('#imageSelector');
-	var selectedImageId = selector.options[selector.selectedIndex].value;
-	var galOwnerId = Template.parentData(0).owner;
-	console.log(galOwnerId);
-	var msg = Template.instance().find('#msg').value;
-	SubmitRequests.insert({
-      from: Meteor.userId(),
-      to: galOwnerId,
-	  gallery: galleryId,
-      image: selectedImageId,
-      message: msg
-    });
-	alert("Your submission has been sent. If the gallery's owner approves it, it will be added to the collection.");
-  Router.go('home');
-	},
-  'click #goHome'(event) {
-	event.preventDefault();
-	console.log(event);
-	Router.go('home');
+		event.preventDefault();
+		console.log(event);
+		var galleryId = Template.parentData(0)._id;	// get the gallery id from the parent template
+		var selectedImageId = $('.selected:checked').map(function() {
+      return this.value;
+    }).get();
+		console.log(selectedImageId);
+		if (selectedImageId.length !== 1) {
+			alert("Please selected strictly one image.");
+			return;
+		}
+		var galOwnerId = Template.parentData(0).owner;
+		var msg = Template.instance().find('#msg').value;
+		SubmitRequests.insert({
+				from: Meteor.userId(),
+				to: galOwnerId,
+			gallery: galleryId,
+				image: selectedImageId[0],
+				message: msg
+			});
+		alert("Your submission has been sent. If the gallery's owner approves it, it will be added to the collection.");
+		Router.go('home');
+		},
+		'click #goHome'(event) {
+		event.preventDefault();
+		console.log(event);
+		Router.go('home');
   },
 });
--- readme.md
+++ readme.md
@@ -42,6 +42,12 @@ DONE:
 
 do all inserting on client side
 
+SECURITY
+https://www.quora.com/Is-meteor-js-secure-Since-everything-is-happening-on-the-client-side-isnt-it-possible-for-someone-to-intercept-or-read-the-DB-calls-in-the-console
+XXS
+http://www.east5th.co/blog/2015/09/07/hijacking-meteor-accounts-with-xss/
+
+
 
 
 creative:
